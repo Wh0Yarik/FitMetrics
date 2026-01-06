9cff93b0752e303655d555b9394debcc93171448
Wh0Yarik <79995109403@ya.ru>
2026-01-06 19:34:30 +0500

    chore: remove legacy api auth module

--- packages/api/src/api/auth/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { registerTrainer } from './auth.controller';
-
-const router = Router();
-
-router.post('/register-trainer', registerTrainer);
-
-export default router;
